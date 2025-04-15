e1c58e9cec767e4cadcfd4d3c880d40cf3d7de1b
var <varshith6946@gmail.com>
2025-04-15 11:26:20 +0530

    css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import supabase from './supabaseConfig/supabaseClient'
-import Insert from './Insert'
+import Insert from './components/Insert'
 
 function App() {
   return (
--- src/components/Insert.jsx
+++ src/components/Insert.jsx
@@ -1,64 +1,74 @@
 import React from 'react'
 import { useState } from 'react'
 import supabase from '../supabaseConfig/supabaseClient'
+import './Insert.css'
 
 function Insert() {
-  const [ name, setName] = useState('')
+  const [name, setName] = useState('')
   const [mail, setMail] = useState('')
   const [message, setMessage] = useState('')
-  const [formError, setFormError] = useState(null)
+  const [formMessage, setFormMessage] = useState('')
 
   const handleSubmit = async (e) => {
     e.preventDefault()
 
-    if(!name || !mail || !message){
-        setFormError('Enter value to all fields');
-        return;
+    if (!name || !mail || !message) {
+      setFormMessage('Enter value to all fields');
+      return;
     }
 
-    const { data,error } = await supabase
-        .from('users')
-        .insert([{name, mail, message}])
-        .select()
+    const { data, error } = await supabase
+      .from('users')
+      .insert([{ name, mail, message }])
+      .select()
 
 
-    if(error){
-        console.log(error);
+    if (error) {
+      console.log(error);
+      setFormMessage(error)
     }
-    if(data){
-        console.log(data);
-        setFormError(null)
+    if (data) {
+      console.log(data);
+      setFormMessage('Response Saved.')
+      setName('')
+      setMail('')
+      setMessage('')
     }
   }
 
 
   return (
-    <div>
-        <form onSubmit={handleSubmit}>
-            <label>Name</label>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+    <div className='container'>
+      <form onSubmit={handleSubmit} className='form'>
+        <label className='name'>Name</label>
+        <input
+          type="text"
+          className='name-input'
+          value={name}
+          placeholder='Enter name..'
+          onChange={(e) => setName(e.target.value)}
+        />
 
-            <label>Mail</label>
-            <input
-              type="mail"
-              value={mail}
-              onChange={(e) => setMail(e.target.value)}
-            />
+        <label className='mail'>Mail</label>
+        <input
+          type="mail"
+          className='mail-input'
+          value={mail}
+          placeholder='Enter mail..'
+          onChange={(e) => setMail(e.target.value)}
+        />
 
-            <label>Message</label>
-            <textarea
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
+        <label className='message'>Message</label>
+        <textarea
+          value={message}
+          className='message-input'
+          placeholder='Enter message..'
+          onChange={(e) => setMessage(e.target.value)}
+        />
 
-            <button type="submit">CLICK ME</button>
-        </form>
-
-        {formError && <div>Error Occured</div>}
+        <button type="submit" className='button'>Submit</button>
+        {formMessage && <div className='form-message'>{formMessage}</div>}
+      </form>
     </div>
   )
 }
